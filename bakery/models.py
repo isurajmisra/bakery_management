99f8b213c8dce34d99fc0d2a42ab82c01ae15799
Suraj Mishra <isurajmisra@gmail.com>
2021-02-26 19:01:07 +0530

    modified models.py

--- bakery/models.py
+++ bakery/models.py
@@ -1,16 +1,17 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class User(AbstractUser):
-    mobile = models.CharField(max_length=15, blank=False, null=True)
-    role = models.CharField(max_length=20, blank=False, null=True)
-
+    username = models.CharField(max_length=50, blank=True, null=True, unique=True)
+    email = models.EmailField(_('email address'), unique=True)
+    mobile = models.CharField(max_length=10)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
         return self.username
-
 
     class Meta:
         db_table = 'user'
@@ -21,7 +22,6 @@ class Ingredient(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.name
 
@@ -31,9 +31,11 @@ class Ingredient(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=50, blank=False, null=True)
-    ingredient = models.ManyToManyField(Ingredient)
-    quantity = models.IntegerField()
-    discount = models.IntegerField()
+    ingredient = models.ManyToManyField(Ingredient, related_name='product_ingredients')
+    ingredient_details = models.JSONField(blank=False, null=True)
+    discount = models.IntegerField(default=0,blank=False, null=True)
+    cost_price = models.FloatField(default=0, blank=False, null=True)
+    selling_price = models.FloatField(default=0, blank=False, null=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -44,15 +46,17 @@ class Product(models.Model):
         db_table = 'product'
 
 
-class ProductPrice(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cost_price = models.FloatField()
-    selling_price = models.FloatField()
-    is_active = models.BooleanField(default=True)
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    products = models.ManyToManyField(Product, related_name='products_order')
+    products_details = models.JSONField(blank=False, null=True)
+    total_price = models.FloatField(default=0, blank=False, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.product.name
+        return self.user.username + '-' + str(self.total_price)
 
     class Meta:
-        db_table = 'product_price'
+        db_table = 'order'
+
+
